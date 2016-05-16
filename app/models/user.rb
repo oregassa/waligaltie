9925af876afdbbd846695d4549ce3de0c16b9100
Olyad Regassa <olyad33@yahoo.com>
2016-05-16 15:42:20 -0500

    added username using devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,25 @@ class User < ActiveRecord::Base
   has_many :comments
   before_save {self.email = email.downcase}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :email, presence: true, length: {maximum: 150},
+  validates :email, presence: true, length: {maximum: 50},
 	           uniqueness: {case_sensitive: false},
 	           format: {with: VALID_EMAIL_REGEX}
+  validates :username, presence: true, length: {minimum: 3, maximum: 12}
+
+  def login=(login)
+    @login = login
+  end
+
+  def login
+  	@login || self.username || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+        where(conditions.to_hash).first
+      end
+    end
 end
