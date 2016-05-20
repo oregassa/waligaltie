58ec2e4dd002fe650529b663f99f80b9b41ac5a6
Olyad Regassa <olyad33@yahoo.com>
2016-05-20 18:34:29 -0500

    modified production

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -10,7 +10,7 @@ class ContactsController < ApplicationController
       flash.now[:error] = nil
     else
       flash.now[:error] = 'Cannot send message.'
-      redirect_to new_contact_path
+      redirect_to :new
     end
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,8 +77,11 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = {host: 'https://waligaltie.herokuapp.com'}
+  config.action_mailer.default_url_options = { host: 'https://waligaltie.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_options = {
+  from: "regassa008@gmail.com" 
+}
 
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
