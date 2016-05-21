d333e5ef03201f8ee50ba510a15f516077a53814
Olyad Regassa <olyad33@yahoo.com>
2016-05-20 22:30:31 -0500

    modified from setter in contact.rb

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,13 +4,13 @@ class ContactsController < ApplicationController
   end
 
   def create
-    @contact=Contact.new(params[:contact])
-    @contact.request=request
+    @contact = Contact.new(params[:contact])
+    @contact.request = request
     if @contact.deliver
       flash.now[:error] = nil
     else
       flash.now[:error] = 'Cannot send message.'
-      redirect_to :new
+      render :new
     end
   end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,15 +1,14 @@
 class Contact < MailForm::Base
   attribute :name,     :validate => true
-  attribute :email
-  validates_format_of  :email, :with => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i  
+  attribute :email     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i  
   attribute :message,  :validate => true
   attribute :nickname, :captcha => true
 
   def headers
     {
-    :subject => "Contact Form",
-    :to => "regassa008@gmail.com",
-    :from => %("#{name}" <#email>)
+      :subject => "Contact Form",
+      :to => "regassa008@gmail.com",
+      :from => %("#{name}" <#{email}>)
     }
   end
 end
