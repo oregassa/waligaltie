7cc49cd85ad0e87be6d82f35668030e5a394ca39
Olyad Regassa <olyad33@yahoo.com>
2016-05-26 11:48:35 -0500

    modified gemfile, contact and production

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,14 +1,14 @@
 class Contact < MailForm::Base
-  attribute :name,     :validate => true
-  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i  
-  attribute :message,  :validate => true
-  attribute :nickname, :captcha => true
+  attribute :name,      :validate => true
+  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :message,   :validate => true
+  attribute :nickname,  :captcha  => true
 
-  def headers 
-  {
-    :subject => "Contact Form",
-    :to => "regassa008@gmail.com",
-    :from => %("#{name}" <#{email}>)
-  }
+  def headers
+    {
+      :subject => "Contact Form",
+      :to => "regassa008@gmail.com",
+      :from => %("#{name}" <#{email}>)
+    }
   end
 end
